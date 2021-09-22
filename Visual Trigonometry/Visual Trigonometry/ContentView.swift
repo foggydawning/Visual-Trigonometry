b5dde73ceb84a94c2cab19aefe3ae16ccad273c4
ILIA CHUB <foggydawning@gmail.com>
2021-09-22 17:48:02 +0300

    Create circle struct

--- Visual Trigonometry/Visual Trigonometry/ContentView.swift
+++ Visual Trigonometry/Visual Trigonometry/ContentView.swift
@@ -8,11 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let spacer_20 = Spacer(minLength: UIScreen.main.bounds.height/20)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack{
+            Spacer(minLength: 15)
+            VStack(alignment: .center){
+                ZStack{
+                    trigonometry_view()
+                }
+                
+                Spacer()
+            }
+            
+            Spacer(minLength: 15)
+            
+        }
     }
 }
+
+struct trigonometry_view: View{
+    var width: CGFloat = UIScreen.main.bounds.width - 30
+    var height: CGFloat {
+        width
+    }
+
+    var body: some View{
+        ZStack{
+            circle(radius: width/2-5, center: CGPoint(x: width/2, y: height/2))
+                .stroke(lineWidth: 3)
+                .frame(alignment: .center)
+                .frame(width: width, height: height, alignment: .center)
+        }
+        
+    }
+}
+
+struct circle: Shape {
+    var radius: CGFloat
+    var center: CGPoint
+    
+    init(radius: CGFloat, center: CGPoint){
+        self.radius = radius
+        self.center = center
+    }
+    
+    func path(in rect: CGRect) -> Path{
+        var path = Path()
+        
+        path.addArc(center: self.center, radius: self.radius, startAngle: Angle(degrees: 0.0), endAngle: Angle(degrees: 360.0), clockwise: true)
+        return path
+    }
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
