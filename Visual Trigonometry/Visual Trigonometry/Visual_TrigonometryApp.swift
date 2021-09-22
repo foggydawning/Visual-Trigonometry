b150f6f269443200dec60b9eaec4a364c3834ffc
ILIA CHUB <foggydawning@gmail.com>
2021-09-22 15:21:38 +0300

    1) .gitignore modified: .DS_Store will ignore 2) create XCode project

---
+++ Visual Trigonometry/Visual Trigonometry/Visual_TrigonometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Visual_TrigonometryApp.swift
+//  Visual Trigonometry
+//
+//  Created by Илья Чуб on 22.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct Visual_TrigonometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
